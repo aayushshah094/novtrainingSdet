455236d428852d8f24aaa0168978d5d1c4d3d849
AayushKumar S Shah <aayushshah094@gmail.com>
2022-11-14 18:29:34 -0500

    first project

---
+++ first.project/src/test/java/SanityTests.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class SanityTests {
+  @Test
+  public void canRunNGTest() {
+  }
+  @Test
+  public void canUseVar() {
+	  String test = "Aayush";
+	  var testOne = "aayush";
+  }
+}
